f315b2e28fe5de3715236aa82170b45d7e5db81b
MirayaReie <mirayareie@gmail.com>
2017-08-19 19:29:28 +0300

    ES6 promises + setting up connection before() tests

--- test/connection.js
+++ test/connection.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 
-// Connect to mongodb
-mongoose.connect('mongodb://localhost/testaroo', { useMongoClient: true });
+// Make mongoose use ES6 promises, instead those outta the box
+mongoose.Promise = global.Promise;
 
-mongoose.connection
-  .once('open', () => {
-    console.log('Connection has been made.');
-  })
-  .on('error', error => {
-    console.log('Connection error', error);
-  });
+// Connect to the db before tests run
+before(done => {
+  // Connect to mongodb
+  mongoose.connect('mongodb://localhost/testaroo', { useMongoClient: true });
+
+  mongoose.connection
+    .once('open', () => {
+      console.log('Connection has been made.');
+      done();
+    })
+    .on('error', error => {
+      console.log('Connection error', error);
+    });
+});
