b0818b68219ab9ebe1c21afb61eecc6d4060d83e
MirayaReie <mirayareie@gmail.com>
2017-08-20 03:01:31 +0300

    Deleting record test added

---
+++ test/deleting_test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const MarioChar = require('../models/mariochar');
+
+// Describe tests
+describe('Deleting records', () => {
+  let char;
+  beforeEach(done => {
+    char = new MarioChar({
+      name: 'Mario'
+    });
+    char.save().then(() => {
+      done();
+    });
+  });
+
+  // Create tests
+  it('Deletes one record from the database', done => {
+    MarioChar.findOneAndRemove({ name: 'Mario' }).then(() => {
+      MarioChar.findOne({ name: 'Mario' }).then(result => {
+        assert(result === null && typeof result === 'object');
+        done();
+      });
+    });
+  });
+});
